8e7b7fe4a445358ae78c99c7c63373d1a64bb495
lyj <756833711@qq.com>
2020-08-13 15:48:56 +0800

    配置样式文件

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    
+    app
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+  @import "~common/stylus/variable"
+
+  #app 
+    color:$color-theme
+</style>
